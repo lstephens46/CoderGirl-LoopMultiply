1fea7607620073b77e55332640b5c64b050240b3
L Stephens <lstephens46@my.stlcc.edu>
2019-04-11 14:37:01 -0500

    finish loopMultiply

--- LoopMultiply/Program.cs
+++ LoopMultiply/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LoopMultiply
 {
@@ -6,13 +7,29 @@ namespace LoopMultiply
     {
         public static void Main()
         {
+
             Console.Write("Enter a number: ");
+            int userInt = Int32.Parse(Console.ReadLine());
+            //List<int> values = new List<int>();
+            int added = 0; 
+            for (int i = 2; i <= 20; i++)
+            {
+                int value = userInt * i;
+                //values.Add(value);
+                Console.WriteLine(value);
+                added += value;
+            }
+            Console.WriteLine(added);
+            Console.ReadLine();
+
+
+
 
             // TODO: Loop from 2 to 20 and multiply the loop value by the number the user entered and output the value each time. 
+            
 
-            // TODO: When the loop is finished, output the sum of all previous values.
-
-            Console.ReadLine();
         }
+
     }
-}
+
+} 
